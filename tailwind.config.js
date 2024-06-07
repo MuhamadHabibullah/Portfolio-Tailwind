b0359febaafab22114149752f6061c58bb174b44
Muhamad Habibullah <148666441+MuhamadHabibullah@users.noreply.github.com>
2024-06-08 04:32:37 +0700

    Update tailwind.config.js
    change the primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
+        primary: '#ff8c1a',
         dark: '#0f172a',
         secondary: '#64748b',
       },
